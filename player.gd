9e930564fd07536ede237fd66e1a52c5b520ad7e
Justin Herrick <jah2488@github.com>
2022-04-02 10:46:11 -0500

    Adding Player and Bullet

---
+++ player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (int) var spread = 10
+
+var Bullet = load("res://bullet.tscn")
+var velocity:Vector2 = Vector2()
+
+func get_input():
+	look_at(get_global_mouse_position())
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+	if Input.is_action_just_released("fire"):
+	  fire_projectile(spread)
+	  fire_projectile(spread * -1)
+	  fire_projectile(0)
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
+func fire_projectile(offset:int):
+	var b = Bullet.instance()
+	print(rotation)
+	b.start($BulletSpawn.global_position, rotation + deg2rad(offset))
+	get_parent().add_child(b)
