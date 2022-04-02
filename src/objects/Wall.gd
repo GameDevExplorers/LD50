46eee3f4299f6f3dcd48630a383bc00329a68188
Vivek Patel <vnp229@gmail.com>
2022-04-02 15:51:18 -0500

    Gate animated

--- src/objects/Wall.gd
+++ src/objects/Wall.gd
@@ -1,25 +1,31 @@
 extends KinematicBody2D
 
-var wall_base = preload("res://src/BlueTexture.jpg")
-var wall_damaged = preload("res://src/DarkBlueTexture.jpg")
-var wall_down = preload("res://src/YellowTexture.jpg")
-
-onready var wall_sprite = $wall_sprite
+onready var solid_gate = $SolidGate
+onready var damaged_gate = $DamagedGate
+onready var broken_gate = $BrokenGate
 
 export var health: = 400
 var _velocity: = Vector2.ZERO
 
+func _ready() -> void:
+	self.remove_child(damaged_gate)
+	self.remove_child(broken_gate)
+
 func repair() -> void:
 	$CollisionShape2D.disabled = false
 	health = 400
-	wall_sprite.set_texture(wall_base)
+	solid_gate.active = true
+	damaged_gate.active = false
+	broken_gate.active = false
 
 func take_damage() -> void:
 	health -= 40
 	if health < 250:
-		wall_sprite.set_texture(wall_damaged)
+		self.remove_child(solid_gate)
+		self.add_child(damaged_gate)
 	if health <= 0:
-		wall_sprite.set_texture(wall_down)
+		self.remove_child(damaged_gate)
+		self.add_child(broken_gate)
 		$CollisionShape2D.disabled = true
 
 func hit() -> void:
