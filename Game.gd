8fa1d4ee710b3d58a6ddac1649d3fb965e020b47
Justin Herrick <jah2488@github.com>
2022-04-02 09:41:28 -0500

    Adding Menus and setting base resolution
    - Main Menu
    - Pause Menu
    - Global "Game" script for handling pauses and unpausing
    - Setting base resolution and scaling ratio

---
+++ Game.gd
@@ -0,0 +1,30 @@
+extends Node
+
+
+export var paused = false
+
+var pause_scene:Control = load("res://pause_menu.tscn").instance()
+
+func current_scene():
+	return get_tree().get_current_scene().get_name()
+
+
+func pause():
+	paused = true
+	get_tree().paused = true
+
+
+func unpause():
+	paused = false
+	get_tree().paused = false
+
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		if current_scene() != "main_menu":
+			if paused == false:
+				pause()
+				get_tree().get_current_scene().add_child(pause_scene)
+				var popup = pause_scene.get_node("PopupDialog") as PopupDialog
+				popup.popup_centered()
+				popup.show()
