7dc3b47e456ddac0052d5793f3db9c6166ea63c9
Vivek Patel <vnp229@gmail.com>
2022-04-02 16:45:44 -0500

    skeleanim

--- src/objects/Wall.gd
+++ src/objects/Wall.gd
@@ -1,31 +1,21 @@
 extends KinematicBody2D
 
-onready var solid_gate = $SolidGate
-onready var damaged_gate = $DamagedGate
-onready var broken_gate = $BrokenGate
+onready var anim = $gate_anim
 
 export var health: = 400
 var _velocity: = Vector2.ZERO
 
-func _ready() -> void:
-	self.remove_child(damaged_gate)
-	self.remove_child(broken_gate)
-
 func repair() -> void:
 	$CollisionShape2D.disabled = false
 	health = 400
-	solid_gate.active = true
-	damaged_gate.active = false
-	broken_gate.active = false
+	anim.set_animation("solid")
 
 func take_damage() -> void:
 	health -= 40
 	if health < 250:
-		self.remove_child(solid_gate)
-		self.add_child(damaged_gate)
+		anim.set_animation("damaged")
 	if health <= 0:
-		self.remove_child(damaged_gate)
-		self.add_child(broken_gate)
+		anim.set_animation("broken")
 		$CollisionShape2D.disabled = true
 
 func hit() -> void:
