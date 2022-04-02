5a55bfff751c912cea6957b153183bec7bac9693
Justin Herrick <jah2488@github.com>
2022-04-02 16:46:50 -0500

    WIP: Additional fire type

--- src/actors/Demon.gd
+++ src/actors/Demon.gd
@@ -6,9 +6,10 @@ export (int) var spread = 10
 # TODO: Add Demon Bullet(s)
 var Bullet = load("res://src/objects/bullet.tscn")
 var velocity:Vector2 = Vector2()
+var player_nearby = false
 
 enum State { IDLE, ATTACKING, ATTACK_PAUSE }
-enum Attack { SPIRAL }
+enum Attack { SPIRAL, BEAM }
 
 var state:int = State.IDLE
 var attack:int = -1
@@ -23,23 +24,45 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	look_at(Game.player_location)
-	if state == State.ATTACKING and attack == Attack.SPIRAL:
-		var count = 10
-		var radius = Vector2(10, 0)
+	var dir = Vector2.ZERO
+	if state == State.ATTACKING:
+		if attack == Attack.SPIRAL:
+			var count = 10
+			var radius = Vector2(10, 0)
+			var step = 2 * PI / count
+			var b = Bullet.instance()
+			b.start(position + radius.rotated(step * attack_progress), 0, speed)
+			get_parent().add_child(b)
+			state = State.ATTACK_PAUSE
+		if attack == Attack.BEAM:
+			fire()
+			fire()
+			state = State.ATTACK_PAUSE
+	else:
+		if !player_nearby:
+			if Game.player_location and position and speed:
+				dir = Game.player_location.direction_to(position)
+				velocity = dir * speed
+		else:
+			velocity = Vector2.ZERO
 
-		var step = 2 * PI / count
-		var b = Bullet.instance()
-		b.start(position + radius.rotated(step * attack_progress), 0, speed)
-		get_parent().add_child(b)
-		state = State.ATTACK_PAUSE
+func _physics_process(delta):
+	velocity = move_and_slide(velocity * delta)
+
+
+func fire():
+	var b = Bullet.instance()
+	b.start(position, 0, speed)
+	get_parent().add_child(b)
+	b.set_target(Game.player_location)
+
 
 func _on_Timer_timeout():
 	if state == State.IDLE:
 		assign_attack()
 		$Timer.start(0.2)
 	if state == State.ATTACK_PAUSE:
-		if attack_progress < 100:
+		if attack_progress < 10:
 			attack_progress += 1
 			state = State.ATTACKING
 			$Timer.start(0.2)
@@ -51,4 +74,15 @@ func _on_Timer_timeout():
 
 func assign_attack():
 	state  = State.ATTACKING
-	attack = Attack.SPIRAL
+	if randf() > 0.5:
+		attack = Attack.BEAM#Attack.SPIRAL
+	else:
+		attack = Attack.BEAM
+
+
+
+func _on_Area2D_body_exited(body:Node):
+	pass # Replace with function body.
+
+func _on_Area2D_body_entered(body:Node):
+	pass # Replace with function body.
--- src/objects/bullet.gd
+++ src/objects/bullet.gd
@@ -2,12 +2,17 @@ extends KinematicBody2D
 
 var speed = 750
 var velocity = Vector2()
+var target = Vector2();
 
 func start(pos, dir, bullet_speed = speed):
 	rotation = dir
 	position = pos
 	speed    = bullet_speed
 	velocity = Vector2(speed, 0).rotated(rotation)
+
+func set_target(pos: Vector2):
+	target = pos
+	velocity = global_position.direction_to(pos) * speed
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
@@ -17,4 +22,8 @@ func _physics_process(delta):
 			collision.collider.hit()
 
 func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_Timer_timeout():
 	queue_free()
