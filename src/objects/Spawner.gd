1ee39d811166745be24ae2a2e208b5a0b5dfaf73
Vivek Patel <vnp229@gmail.com>
2022-04-02 20:38:46 -0500

    Spawn location randomization

--- src/objects/Spawner.gd
+++ src/objects/Spawner.gd
@@ -4,9 +4,11 @@ export var defaultTarget: NodePath
 export var target: NodePath
 
 var Mob = load("res://src/actors/Mob.tscn")
+var rng = RandomNumberGenerator.new()
 
 onready var default_target_node = get_node(defaultTarget)
 onready var target_node = get_node(target)
+
 
 func _ready() -> void:
 	$Timer.start(5)
@@ -15,7 +17,11 @@ func _on_Timer_timeout() -> void:
 	spawn()
 
 func spawn() -> void:
+	rng.randomize()
 	var m = Mob.instance()
 	m.set_sigils(target_node, default_target_node)
-	m.global_position = global_position
+	var ran_1 = rng.randf_range(-100.0, 100.0)
+	var ran_2 = rng.randf_range(-100.0, 100.0)
+	print(str(ran_1) + ":" + str(ran_2))
+	m.global_position = global_position + Vector2(ran_1, ran_2)
 	get_parent().add_child(m)
