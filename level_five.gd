9a1710a2da45206db073b9d6db3ba29f9318c5b1
Vivek Patel <vnp229@gmail.com>
2022-04-03 19:04:53 -0500

    Lefel 5 config

---
+++ level_five.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+export (Array, int) var spawn_count = [10, 10, 15, 0 ,0 , 20, 20, 25, 0, 0, 25, 25, 30, 0, 0, 20, 20, 25, 25, 30, 30]
+export var spawn_timer = 10
+var Demon = load("res://src/actors/Demon.tscn")
+
+signal demon_summoned
+
+signal spawn_skeles(spawn_arr)
+
+var rng = RandomNumberGenerator.new()
+var tick_count = 0
+
+func _ready():
+	print("hello?")
+	$Timer.start(1)
+
+
+func _on_Timer_timeout():
+	tick_count = tick_count + 1
+	handle_spawns()
+	$Timer.start(1)
+	$Label.text = "Demon Time in: " + str(Game.elapsed_time() - Game.summon_timer) + " seconds"
+	if Game.elapsed_time() > Game.summon_timer and Game.demon_summoned == false:
+		Game.demon_summoned = true
+		emit_signal("demon_summoned")
+		var d = Demon.instance()
+		d.position = $EternalSigil.position
+		add_child(d)
+
+func handle_spawns():
+	if tick_count % spawn_timer == 0:
+		var new_arr = [0, 0, 0, 0, 0]
+		
+		rng.randomize()
+		var divide = rng.randi_range(1, 5)
+
+		var index = (tick_count / spawn_timer) - 1
+		if index > spawn_count.size():
+			index = 8
+		var spawns = spawn_count[index]
+		
+		var each_node = spawns / divide
+		
+		for n in range(divide):
+			var i = rng.randi_range(0, 4)
+			new_arr[i] = new_arr[i] + each_node
+		
+		emit_signal("spawn_skeles", new_arr)
--- main_menu.gd
+++ main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 func _on_StartBtn_pressed():
 	$Select.play()
 	yield(get_tree().create_timer(0.4), "timeout")
-	var result = get_tree().change_scene("res://level_one.tscn")
+	var result = get_tree().change_scene("res://level_five.tscn")
 	if result != OK:
 		print("Error loading scene")
 
