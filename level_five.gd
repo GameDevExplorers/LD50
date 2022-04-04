94cbc79ca2d2cc5109368a117e2ca0a8d82f1c7c
Vivek Patel <vnp229@gmail.com>
2022-04-04 10:13:52 -0500

    Fix bug spawner

--- level_five.gd
+++ level_five.gd
@@ -12,7 +12,6 @@ var rng = RandomNumberGenerator.new()
 var tick_count = 0
 
 func _ready():
-	print("hello?")
 	$Timer.start(1)
 
 
@@ -39,11 +38,10 @@ func handle_spawns():
 		if index >= spawn_count.size():
 			index = 8
 		var spawns = spawn_count[index]
-		
 		var each_node = spawns / divide
 		
 		for n in range(divide):
 			var i = rng.randi_range(0, 4)
 			new_arr[i] = new_arr[i] + each_node
-		
+
 		emit_signal("spawn_skeles", new_arr)
