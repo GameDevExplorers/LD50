7f91e04a34284a0a5601f5707b50a178f823f71d
Justin Herrick <jah2488@github.com>
2022-04-04 08:52:02 -0500

    Bullet casings now are behind player after landing

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -51,9 +51,6 @@ func get_input():
 	if Input.is_action_just_pressed("fire"):
 		$BulletSpawn/MuzzleFlash.frame = 1
 		$GunFire.play()
-		var c = Casing.instance()
-		c.position = $BulletSpawn.global_position
-		get_parent().add_child(c)
 		yield(get_tree().create_timer(0.05), "timeout")
 		fire_projectile(0)
 		$BulletSpawn/MuzzleFlash.frame = 0
@@ -94,6 +91,9 @@ func _physics_process(delta):
 	Game.player_location = global_position
 
 func fire_projectile(offset:int):
+	var c = Casing.instance()
+	c.position = $CasingSpawn.global_position
+	get_node("../CasingContainer").add_child(c)
 	$BulletSpawn/MuzzleFlash.frame = 0
 	var b = Bullet.instance()
 	b.start($BulletSpawn.global_position, get_angle_to(get_global_mouse_position()) + deg2rad(offset), "player")
