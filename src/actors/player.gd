9967cfde623ee6f9aec0701d502632052ab52797
Matt Rice <matt.rice@kin.com>
2022-04-10 09:58:57 -0500

    Revert "Can now roll"

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -8,22 +8,11 @@ export (int) var max_health = 400
 var Bullet = load("res://src/objects/bullet.tscn")
 var Casing = load("res://src/objects/casing.tscn")
 var velocity:Vector2 = Vector2()
-var roll_vector:Vector2 = Vector2.ZERO
-
 var ready_to_fire = true
 var invincible = false
 
 var cross_hair:Vector2 = Vector2()
 
-enum {
-	MOVE,
-	ROLL
-}
-
-var state = MOVE
-
-onready var anim = $PlayerAnimation
-onready var player_shadow = $PlayerAnimation/PlayerShadow
 onready var health_bar = $Healthbar
 
 func _ready():
@@ -36,17 +25,17 @@ func _ready():
 func _process(_delta):
 	handle_directional_camera()
 	if get_global_mouse_position().x < global_position.x:
-		anim.flip_h = true
+		$AnimatedSprite.flip_h = true
 		$BulletSpawn.position.x = -22
 		$BulletSpawn/MuzzleFlash.flip_h = true
 		$BulletSpawn/MuzzleFlash.position.x = -20
-		player_shadow.position.x = 2
+		$AnimatedSprite/Sprite.position.x = 2
 	else:
-		anim.flip_h = false
+		$AnimatedSprite.flip_h = false
 		$BulletSpawn.position.x = 22
 		$BulletSpawn/MuzzleFlash.flip_h = false
 		$BulletSpawn/MuzzleFlash.position.x = 20
-		player_shadow.position.x = -3
+		$AnimatedSprite/Sprite.position.x = -3
 
 func _on_demon_summoned():
 	add_child(health_bar)
@@ -65,42 +54,22 @@ func handle_directional_camera():
 	$Camera2D.offset.y = y
 
 
-func move_state():
-	var input_vector = Vector2.ZERO
+func get_input():
+	velocity = Vector2()
 	if Input.is_action_pressed("right"):
-		input_vector.x += 1
+		velocity.x += 1
 	if Input.is_action_pressed("left"):
-		input_vector.x -= 1
+		velocity.x -= 1
 	if Input.is_action_pressed("down"):
-		input_vector.y += 1
+		velocity.y += 1
 	if Input.is_action_pressed("up"):
-		input_vector.y -= 1
-		
-	velocity = input_vector.normalized() * speed
-	
-	if velocity.length_squared() > 0:
-		roll_vector = velocity
-		if $Walk.playing == false:
-			$Walk.play()
-		anim.play("move")
-	else:
-		anim.stop()
-		anim.frame = 0
+		velocity.y -= 1
 
-	handle_attack()
+	velocity = velocity.normalized() * speed
 
-	if Input.is_action_just_pressed("roll"):
-		state = ROLL
+	if invincible:
+		return
 
-func roll_state():
-	velocity = roll_vector.normalized() * speed * 1.75
-	handle_attack()
-	anim.play("roll")
-	invincible = true
-	modulate = Color.gray
-
-
-func handle_attack():
 	if Input.is_action_just_pressed("fire"):
 		$BulletSpawn/MuzzleFlash.frame = 1
 		$GunFire.play()
@@ -130,43 +99,46 @@ func handle_attack():
 		fire_projectile(0)
 		fire_projectile((spread / 3) * -1)
 
-
-
 func _physics_process(delta):
-	match state:
-		MOVE:
-			move_state()
-		
-		ROLL:
-			roll_state()
-		
+	# var direction: Vector2 = Input.get_vector("nav-left", "nav-right", "nav-down", "nav-up")
+
+	# var movement = direction * delta * 500
+	# if (movement):
+		# cross_hair += movement
+		# get_viewport().warp_mouse(cross_hair)
+		# print("movement: ", movement)
+
+	get_input()
+	if velocity.length_squared() > 0:
+		if $Walk.playing == false:
+			$Walk.play()
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		$AnimatedSprite.frame = 0
+
 	velocity = move_and_slide(velocity)
 	Game.player_location = global_position
 	if health <= 0:
-		game_over()
+		var result = get_tree().change_scene("res://game_over.tscn")
+		if result != OK:
+			print_debug("Failed to change scene: " + result)
 
 func fire_projectile(offset:int):
 	if health <= 1:
 		return
-
-	drop_casing()
-	show_bullet(offset)
-
+	var c = Casing.instance()
+	c.position = $CasingSpawn.global_position
+	if $AnimatedSprite.flip_h == true:
+		c.flip_h = true
+	$CasingContainer.add_child(c)
+	$BulletSpawn/MuzzleFlash.frame = 0
+	var b = Bullet.instance()
+	b.start($BulletSpawn.global_position, get_angle_to(get_global_mouse_position()) + deg2rad(offset), "player")
+	get_parent().add_child(b)
 	health -= 1
 	health_bar.set_health(health)
 
-func drop_casing():
-	var casing = Casing.instance()
-	casing.position = $CasingSpawn.global_position
-	if anim.flip_h == true:
-		casing.flip_h = true
-	$CasingContainer.add_child(casing)
-
-func show_bullet(offset):
-	$BulletSpawn/MuzzleFlash.frame = 0
-	var bullet = Bullet.instance()
-	bullet.start($BulletSpawn.global_position, get_angle_to(get_global_mouse_position()) + deg2rad(offset), "player")
-	get_parent().add_child(bullet)
 
 func _on_GunFire_finished():
 	$GunReload.play()
@@ -193,7 +165,9 @@ func take_damage(damage) -> void:
 	modulate = Color.white
 
 	if health <= 0:
-		game_over()
+		var result = get_tree().change_scene("res://game_over.tscn")
+		if result != OK:
+			print_debug("Failed to change scene: " + result)
 	else:
 		yield(get_tree().create_timer(0.1), "timeout")
 		modulate = Color.red
@@ -203,28 +177,8 @@ func take_damage(damage) -> void:
 		modulate = Color.white
 		invincible = false
 
-
-func game_over():
-	for _i in range(5):
-		yield(get_tree().create_timer(0.1), "timeout")
-		modulate = Color.red
-		yield(get_tree().create_timer(0.1), "timeout")
-		modulate = Color.gray
-	yield(get_tree().create_timer(0.25), "timeout")
-	var result = get_tree().change_scene("res://game_over.tscn")
-	if result != OK:
-		print_debug("Failed to change scene: " + result)
-
-	
 func _on_BulletCollider_body_entered(body: Node) -> void:
 	if body.get("damage") && body.get("spawned_by") != "player":
 		body.queue_free()
 		if invincible == false:
 			take_damage(body.get("damage"))
-
-
-func _on_player_anim_finished() -> void:
-	state = MOVE
-	invincible = false
-	modulate = Color.white
-
