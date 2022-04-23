c8f83db0361337900cec6bbd6a63a6766da2df38
Matt Rice <mattrice12@outlook.com>
2022-04-23 16:49:44 -0500

    Adds damage numbers

---
+++ src/actors/DmgNumbersManager.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var DamageNumbers = preload("res://src/objects/DamageNumbers.tscn")
+
+export var travel = Vector2(0, -40)
+export var duration = 1
+export var spread = PI/2
+
+func show_value(value, crit = false):
+    var damage_numbers = DamageNumbers.instance()
+    add_child(damage_numbers)
+    damage_numbers.show_value(str(value), travel, duration, spread, crit)
+    print("DmgNumbersManager")
